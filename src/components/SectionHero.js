7b95b014a805161aafdeb1970a36eef54efdefe2
Stackbit Code Editor <projects@stackbit.com>
2021-04-10 23:14:46 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -29,7 +29,7 @@ export default class SectionHero extends React.Component {
               </div>
                 {_.get(section, 'image', null) && (
                   <div className="cell block-preview">
-                    <img src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} />
+                    <img src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} width />
                   </div>
                   )}
             </section>
